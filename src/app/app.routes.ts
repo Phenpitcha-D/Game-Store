e12279501bbc451f781c3ea2e16f08bb9cfa0e8e
Phenpitcha-D <ppphenpitcha@gmail.com>
2025-10-04 19:01:50 +0700

    Profile-Wallet UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/pages/login/login';
+import { ProfileWallet } from './components/pages/profile-wallet/profile-wallet';
 
 export const routes: Routes = [
     { path : 'login', component: Login},
+    {path: 'profile-wallet', component: ProfileWallet },
 ];
